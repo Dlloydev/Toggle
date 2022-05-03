5852adb3cefa8f9cd96ee9c852d19fc88c612930
Dlloydev <dlloydev@testcor.ca>
2022-05-03 17:46:04 -0400

    Update

--- examples/Toggle_Basic/Toggle_Basic.ino
+++ examples/Toggle_Basic/Toggle_Basic.ino
@@ -1,6 +1,6 @@
 #include <Toggle.h>
 
-Toggle sw1(3, 4);
+Toggle sw1(7, 8);
 
 void setup() {
   Serial.begin(115200);
@@ -9,10 +9,7 @@ void setup() {
 void loop() {
   sw1.poll();
 
-  if      (sw1.isUp())    Serial.println(F("Up"));
-  else if (sw1.isMid())   Serial.println(F("Mid"));
-  else if (sw1.isDn())    Serial.println(F("Dn"));
-  else if (sw1.upToMid()) Serial.println(F("Up⇒Mid"));
+  if      (sw1.upToMid()) Serial.println(F("Up⇒Mid"));
   else if (sw1.dnToMid()) Serial.println(F("Dn⇒Mid"));
   else if (sw1.midToUp()) Serial.println(F("Mid⇒Up"));
   else if (sw1.midToDn()) Serial.println(F("Mid⇒Dn"));
